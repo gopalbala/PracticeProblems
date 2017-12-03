11d16abe97dfbf7a3c3ab0a9b799c48af5706804
Gopalakrishnan <balasubramaniangopalakrishnan@gmail.com>
2017-12-03 11:55:10 +0530

    added print matrix diagonally

--- src/main/java/PrintMatrixDiagonal.java
+++ src/main/java/PrintMatrixDiagonal.java
@@ -2,4 +2,28 @@
  * Created by gbalasubrmanian on 03/12/17.
  */
 public class PrintMatrixDiagonal {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        printMAtrixDiagonally(a);
+    }
+
+    static void printMAtrixDiagonally(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            int i1 = i;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(mat[i1][j] + " ");
+                i1 = i1 - 1;
+            }
+            System.out.println();
+        }
+        int row = mat.length - 1;
+        for (int i = 1; i < mat.length; i++) {
+            int i2 = row;
+            for (int j1 = i; j1 <= mat.length - 1; j1++) {
+                System.out.print(mat[i2][j1] + " ");
+                i2 = i2 - 1;
+            }
+            System.out.println();
+        }
+    }
 }
